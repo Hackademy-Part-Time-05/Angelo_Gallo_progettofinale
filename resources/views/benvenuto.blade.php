b60ecf9523c04916ce392800ec30923e543fccd4
angelogallo97 <angelogallo1997@gmail.com>
2023-08-10 19:20:27 +0200

    Initial commit

---
+++ resources/views/benvenuto.blade.php
@@ -0,0 +1,12 @@
+<!-- resources/views/benvenuto.blade.php -->
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Benvenuto</title>
+</head>
+<body>
+    <h1>Benvenuto</h1>
+    <p>{{ $messaggio }}</p>
+</body>
+</html>
